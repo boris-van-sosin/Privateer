5952da848355009624fd272b62ba69146c90ef74
boris-van-sosin <sosin@csf.technion.ac.il>
2018-09-24 02:08:58 +0300

    Migrated to Unity 2018
    It was hell.

--- Assets/World objects/Scripts/StatusSubsystem.cs
+++ Assets/World objects/Scripts/StatusSubsystem.cs
@@ -27,21 +27,10 @@ public class StatusSubsystem : MonoBehaviour
 
     private void UpdateHitPointsDisplay()
     {
-        _image.color = _colorGradient.Evaluate(((float)_attachedComponent.ComponentHitPoints) / _attachedComponent.ComponentMaxHitPoints);
+        _image.color = ColorGradient.Evaluate(((float)_attachedComponent.ComponentHitPoints) / _attachedComponent.ComponentMaxHitPoints);
     }
 
     private IShipActiveComponent _attachedComponent;
     private UnityEngine.UI.Image _image;
-    private static readonly Gradient _colorGradient = new Gradient()
-    {
-        colorKeys = new GradientColorKey[]
-        {
-            new GradientColorKey(Color.black, 0.0f),
-            new GradientColorKey(Color.red, 0.1f),
-            new GradientColorKey(new Color(249.0f/255.0f, 170.0f/255.0f, 6.0f/255.0f), 0.25f),
-            new GradientColorKey(Color.yellow, 0.5f),
-            new GradientColorKey(Color.green, 1f)
-        },
-        mode = GradientMode.Blend
-    };
+    public Gradient ColorGradient;
 }
