3233015b3f2d8ba480a320de99c0902443141f98
boris-van-sosin <sosin@csf.technion.ac.il>
2019-04-10 22:58:10 +0300

    Implemented Bspline curve
    Includes a test.
    Also created an assembly definition, to allow testing.

---
+++ Assets/World objects/Scripts/Math Tests/BsplineTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class BsplineTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void BsplineTestSimplePasses()
+        {
+            Vector3[] ctlMesh = new Vector3[]
+            {
+                new Vector3(0, 0, 0),
+                new Vector3(0, 1, 0.7f),
+                new Vector3(0, 1, 0.7f + 1.5f),
+                new Vector3(0, 0, 0.7f + 1.5f + 0.7f)
+            };
+            BSplineCurve<Vector3> bsp = BSplineCurve<Vector3>.UniformOpen(ctlMesh, 3, AddVec, VecMultScalar);
+            Assert.IsTrue(bsp.Eval(0f) == new Vector3(0, 0, 0));
+            Assert.IsTrue(bsp.Eval(0.2f) == new Vector3(0, 0.64f, 0.568f));
+            Assert.IsTrue(bsp.Eval(0.45f) == new Vector3(0, 0.99f, 1.3005f));
+            Assert.IsTrue(bsp.Eval(0.5f) == new Vector3(0, 1f, 1.45f));
+            Assert.IsTrue(bsp.Eval(0.86f) == new Vector3(0, 0.4816f, 2.50408f));
+            Assert.IsTrue(bsp.Eval(1f) == new Vector3(0, 0, 2.9f));
+        }
+
+        private static Vector3 AddVec(Vector3 v1, Vector3 v2)
+        {
+            return v1 + v2;
+        }
+
+        private static Vector3 VecMultScalar(Vector3 v1, float f)
+        {
+            return v1 * f;
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator BsplineTestWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
---
+++ Assets/World objects/Scripts/SplineUtils.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class BSplineCurve<T>
+{
+    public BSplineCurve(IEnumerable<T> CtlPoints, IEnumerable<float> KV, Func<T, T, T> add, Func<T, float, T> multScalar)
+    {
+        _ctlMesh = CtlPoints.ToArray();
+        _kv = KV.ToArray();
+        if (_kv.Length <= 1)
+        {
+            throw new Exception("Attempted to create B-spline in which empty knot vector");
+        }
+        Order = _kv.Length - _ctlMesh.Length;
+        if (Order < 0)
+        {
+            throw new Exception("Attempted to create B-spline in which order is greater than length");
+        }
+        _add = add;
+        _multScalar = multScalar;
+    }
+
+    public static BSplineCurve<T> UniformOpen(IEnumerable<T> CtlPoints, int order, Func<T, T, T> add, Func<T, float, T> multScalar)
+    {
+        int length = CtlPoints.Count();
+        if (length < order)
+        {
+            throw new Exception("Attempted to create B-spline in which order is greater than length");
+        }
+        return new BSplineCurve<T>(CtlPoints, CreateOpenKV(order, length), add, multScalar);
+    }
+
+    private static float[] CreateOpenKV(int order, int ctlMeshLen)
+    {
+        int kvLen = ctlMeshLen + order;
+        int interiorKnots = ctlMeshLen - order + 1;
+        float[] kv = new float[kvLen];
+        int i = 1, j = ctlMeshLen - order, k = 0;
+        while (k < order)
+        {
+            kv[k] = 0;
+            ++k;
+        }
+        while (i <= j)
+        {
+            kv[k] = ((float)i) / interiorKnots;
+            ++k;
+            ++i;
+        }
+        for (j = 0; j < order; j++)
+        {
+            kv[k++] = ((float)i) / interiorKnots;
+        }
+        return kv;
+    }
+
+    public T Eval(float x)
+    {
+        T[] d = new T[Order];
+        int k = FindKnotInterval(x);
+        for (int i = 0; i < Order; ++i)
+        {
+            d[i] = _ctlMesh[i + k - (Order - 1)];
+        }
+        for (int r = 1; r < Order; ++r)
+        {
+            for (int j = Order - 1; j >= r; --j)
+            {
+                float alpha = (x - _kv[j + k - (Order - 1)]) / (_kv[j + 1 + k - r] - _kv[j + k - (Order - 1)]);
+                d[j] = _add(_multScalar(d[j - 1], 1 - alpha), _multScalar(d[j], alpha));
+            }
+        }
+        return d[Order - 1];
+    }
+
+    private int FindKnotInterval(float x)
+    {
+        if (x < _kv[0])
+        {
+            throw new Exception("parameter outside domain of B-spline");
+        }
+        for (int i = 0; i < _kv.Length - 1; ++i)
+        {
+            if (_kv[i] <= x && x < _kv[i + 1] || (x <= _kv[i + 1] && Mathf.Approximately(_kv[i + 1], _kv.Last())))
+            {
+                return i;
+            }
+        }
+        throw new Exception("parameter outside domain of B-spline");
+    }
+
+    public readonly int Order;
+    private readonly T[] _ctlMesh;
+    private readonly float[] _kv;
+    private readonly Func<T, T, T> _add;
+    private readonly Func<T, float, T> _multScalar;
+}
