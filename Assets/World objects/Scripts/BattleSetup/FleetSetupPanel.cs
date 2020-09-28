8c4c0e4c1faf3f924e163855fdbae9b23317d762
boris-van-sosin <boris.van.sosin@gmail.com>
2020-09-28 20:19:17 +0300

    Started creating battle setup scene
    It has most of the component, but no actual code yet.

---
+++ Assets/World objects/Scripts/BattleSetup/FleetSetupPanel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FleetSetupPanel : MonoBehaviour
+{
+    public RectTransform AvailableShipsScrollContent;
+    public RectTransform SelectedShipsScrollContent;
+    public Image CurrSelectedShipImage;
+    public RectTransform AvailableShipItemTemplate;
+    public RectTransform SelectedShipItemTemplate;
+}
